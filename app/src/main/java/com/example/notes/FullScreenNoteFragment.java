7e1eefd0632f671344492e3d7bcb6c46e1739494
katerinabuhalova <katerina.buhalova@gmail.com>
2021-05-02 14:57:53 +0300

    [ WIP] impliment notes functionality in portrait orientation

---
+++ app/src/main/java/com/example/notes/FullScreenNoteFragment.java
@@ -0,0 +1,23 @@
+package com.example.notes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FullScreenNoteFragment extends Fragment {
+
+    public FullScreenNoteFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_full_screen_note, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/notes/NoteDetailsFragment.java
@@ -0,0 +1,60 @@
+package com.example.notes;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class NoteDetailsFragment extends Fragment {
+
+    public NoteDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_note_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initList(view);
+    }
+
+    private void initList(View view) {
+        LinearLayout layoutView = (LinearLayout) view;
+        String[] notes = getResources().getStringArray(R.array.notes);
+        for (int i = 0; i < notes.length; i++) {
+            String note = notes[i];
+            TextView tv = new TextView(getContext());
+            tv.setText(note);
+            tv.setTextSize(50);
+            layoutView.addView(tv);
+            final int fi = i;
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showActivityTwo();
+                }
+            });
+        }
+    }
+
+    private void showActivityTwo() {
+        Intent intent = new Intent();
+        intent.setClass(getActivity(), NoteDetailsActivity.class);
+        startActivity(intent);
+    }
+}
+
