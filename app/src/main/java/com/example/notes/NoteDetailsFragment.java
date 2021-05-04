7a0e25b2cfd614661225ff0f3d959ab29f3ae6ca
katerinabuhalova <katerina.buhalova@gmail.com>
2021-05-04 14:25:12 +0300

    implement  landscape  orientation

--- app/src/main/java/com/example/notes/NoteDetailsFragment.java
+++ app/src/main/java/com/example/notes/NoteDetailsFragment.java
@@ -1,11 +1,14 @@
 package com.example.notes;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +18,8 @@ import android.widget.TextView;
 
 
 public class NoteDetailsFragment extends Fragment {
+
+    private boolean isLandscape;
 
     public NoteDetailsFragment() {
         // Required empty public constructor
@@ -42,9 +47,10 @@ public class NoteDetailsFragment extends Fragment {
             tv.setTextSize(50);
             layoutView.addView(tv);
             final int fi = i;
-            tv.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+            tv.setOnClickListener(v -> {
+                if(isLandscape){
+                    showFragment();
+                } else {
                     showActivityTwo();
                 }
             });
@@ -56,5 +62,21 @@ public class NoteDetailsFragment extends Fragment {
         intent.setClass(getActivity(), NoteDetailsActivity.class);
         startActivity(intent);
     }
-}
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        isLandscape = getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE;
+    }
+
+    private void showFragment() {
+        FullScreenNoteFragment details = new FullScreenNoteFragment();
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container_land, details);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
+    }
+}
