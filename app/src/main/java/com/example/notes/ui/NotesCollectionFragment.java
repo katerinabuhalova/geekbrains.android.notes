e9db1e31278cf49dd3c963431385bfb5a007516d
katerinabuhalova <katerina.buhalova@gmail.com>
2021-05-18 09:25:42 +0300

    imlement alertDialog

--- app/src/main/java/com/example/notes/ui/NotesCollectionFragment.java
+++ app/src/main/java/com/example/notes/ui/NotesCollectionFragment.java
@@ -1,5 +1,6 @@
 package com.example.notes.ui;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -20,6 +22,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.notes.DetailsFragment;
+import com.example.notes.MainActivity;
 import com.example.notes.R;
 import com.example.notes.date.Note;
 import com.example.notes.date.NotesSource;
@@ -55,11 +58,11 @@ public class NotesCollectionFragment  extends Fragment {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_add:
                 data.addNote(new Note("Note" + data.size(),
                         "description" + data.size(),
-                Calendar.getInstance().getTime().toString()));
+                        Calendar.getInstance().getTime().toString()));
                 adapter.notifyItemInserted(data.size() - 1);
                 recyclerView.scrollToPosition(data.size() - 1);
                 return true;
@@ -97,8 +100,7 @@ public class NotesCollectionFragment  extends Fragment {
         });
     }
 
-    private void openDetails(NotesSource data, int position)
-    {
+    private void openDetails(NotesSource data, int position) {
         FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         DetailsFragment detailsFragment = DetailsFragment.newInstance(data, position);
@@ -117,7 +119,7 @@ public class NotesCollectionFragment  extends Fragment {
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         int position = adapter.getMenuPosition();
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.action_update:
                 data.updateNote(position,
                         new Note("Updated note" + position,
@@ -126,10 +128,45 @@ public class NotesCollectionFragment  extends Fragment {
                 adapter.notifyItemChanged(position);
                 return true;
             case R.id.action_delete:
-                data.deleteNote(position);
-                adapter.notifyItemRemoved(position);
+                //  data.deleteNote(position);
+                // adapter.notifyItemRemoved(position);
+                snowDeleteDialog(position);
                 return true;
         }
         return super.onContextItemSelected(item);
+    }
+
+    public void snowDeleteDialog( int position) {
+        // Создаём билдер и передаём контекст приложения
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // В билдере указываем заголовок окна. Можно указывать как ресурс,
+        // так и строку
+        builder.setTitle(R.string.exclamation)
+                // Указываем сообщение в окне. Также есть вариант со
+                // строковым параметром
+                .setMessage(R.string.press_button)
+                // Можно указать и пиктограмму
+                .setIcon(R.mipmap.ic_launcher_round)
+                // Из этого окна нельзя выйти кнопкой Back
+                .setCancelable(false)
+                // Устанавливаем кнопку. Название кнопки также можно
+                // задавать строкой
+                .setNegativeButton(R.string.button1,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        })
+                .setPositiveButton(R.string.button,
+                        // Ставим слушатель, нажатие будем обрабатывать
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Toast.makeText(getActivity(), "Заметка удалена", Toast.LENGTH_SHORT).show();
+                                data.deleteNote(position);
+                                adapter.notifyItemRemoved(position);
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
